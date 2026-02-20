7b17aa6410dbbc433f06b6aedfa7ee23abdcca9c
Anonav0 <swarnavokhanra@gmail.com>
2026-02-20 22:34:12 +0530

    v7: saving the best car after manual iterations

--- main.js
+++ main.js
@@ -9,6 +9,10 @@ const road = new Road(carCanvas.width / 2, carCanvas.width * 0.9);
 const N = 100;
 const cars = generateCars(N);
 const traffic = [new Car(road.getLaneCenter(1), -100, 30, 50, "DUMMY", 2)];
+let bestCar = cars[0];
+if (localStorage.getItem("bestBrain")) {
+  bestCar.brain = JSON.parse(localStorage.getItem("bestBrain"));
+}
 
 function animate(time) {
   for (let i = 0; i < traffic.length; i++) {
@@ -18,7 +22,7 @@ function animate(time) {
     cars[i].update(road.borders, traffic);
   }
 
-  const bestCar = cars.find((c) => c.y == Math.min(...cars.map((c) => c.y)));
+  bestCar = cars.find((c) => c.y == Math.min(...cars.map((c) => c.y)));
 
   carCanvas.height = window.innerHeight;
   networkCanvas.height = window.innerHeight;
@@ -49,6 +53,13 @@ function generateCars(N) {
     cars.push(new Car(road.getLaneCenter(1), 100, 30, 50, "AI"));
   }
   return cars;
+}
+
+function save() {
+  localStorage.setItem("bestBrain", JSON.stringify(bestCar.brain));
+}
+function discard() {
+  localStorage.removeItem("bestBrain");
 }
 
 animate();
